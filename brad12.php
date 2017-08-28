f4a0d678d39f8d41490e82480be0ad08f857a904
Brad <brad@brad.tw>
2017-08-28 16:13:28 +0800

    v10

---
+++ brad12.php
@@ -0,0 +1,8 @@
+<?php
+    $a = 10;
+    switch (++$a){
+        case 9: echo "A:$a"; break;
+        case 10: echo "B:$a"; break;
+        case 11: echo "C:$a"; break;
+        case 12: echo "D:$a"; break;
+    }
