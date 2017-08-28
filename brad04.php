d9c00f341bbf6b37eb43c85a86b1784ec372baf6
Brad <brad@brad.tw>
2017-08-28 14:15:46 +0800

    v4

--- brad04.php
+++ brad04.php
@@ -1,14 +1,16 @@
 <?php
+    $x = $y = $result = '';
     if (isset($_GET['x'])){
         $x = $_GET['x'];
         $y = $_GET['y'];
         $result = $x + $y;
-        echo "==> $x + $y = $result";
+        //echo "==> $x + $y = $result";
     }
 ?>
 <form>
-    <input type="text" name="x" />
+    <input type="text" name="x" value="<?php echo $x; ?>"/>
     +
-    <input type="text" name="y" />
+    <input type="text" name="y" value="<?php echo $y; ?>"/>
     <input type="submit" value="=" />
+    <?php echo $result ?>
 </form>
