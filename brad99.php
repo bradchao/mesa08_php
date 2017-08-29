95c4edd3af4fa9e1453cd6350a9cfb8645538000
Brad <brad@brad.tw>
2017-08-29 09:33:00 +0800

    v13

--- brad99.php
+++ brad99.php
@@ -1,40 +1,24 @@
 <table border="1" width="100%">
-    <tr>
-        <?php
-            for ($j=2; $j<=5; $j++){
-                echo '<td>';
-                for ($i=1; $i<=9; $i++){
-                    $r = $j * $i;
-                    echo "{$j} x {$i} = {$r}<br>";
-                }
-                echo '</td>';
+<?php
+    for ($k=0; $k<4; $k++){
+        echo '<tr>';
+        for ($j=2; $j<=5; $j++){
+            $newj = $j + 4 * $k;
+
+            if (($j+$k) % 2 == 0){
+                echo '<td bgcolor="#d2691e">';
+            }else{
+                echo '<td bgcolor="#ffff00">';
             }
-        ?>
-    </tr>
-    <tr>
-        <td>
-            2 x 1 = 2<br>
-            2 x 1 = 2<br>
-            2 x 1 = 2<br>
-            2 x 1 = 2<br>
-        </td>
-        <td>
-            2 x 1 = 2<br>
-            2 x 1 = 2<br>
-            2 x 1 = 2<br>
-            2 x 1 = 2<br>
-        </td>
-        <td>
-            2 x 1 = 2<br>
-            2 x 1 = 2<br>
-            2 x 1 = 2<br>
-            2 x 1 = 2<br>
-        </td>
-        <td>
-            2 x 1 = 2<br>
-            2 x 1 = 2<br>
-            2 x 1 = 2<br>
-            2 x 1 = 2<br>
-        </td>
-    </tr>
+
+
+            for ($i=1; $i<=9; $i++){
+                $r = $newj * $i;
+                echo "{$newj} x {$i} = {$r}<br>";
+            }
+            echo '</td>';
+        }
+        echo '</tr>';
+    }
+?>
 </table>
