ac9220d4ba619fc1bbec343c5f2518f84ce188d6
Brad <brad@brad.tw>
2017-08-29 14:18:12 +0800

    v19

--- pokerv4.php
+++ pokerv4.php
@@ -4,18 +4,25 @@
     shuffle($poker);
 
     // 發牌
-    $players = array(array(),array(),array(),array());
     foreach ($poker as $k => $v) {
         $players[$k%4][(int)($k/4)] = $v;
     }
 
     // 攤牌
+    $patterns = array('&spades;',
+        '<font color="red">&hearts;</font>',
+        '<font color="red">&diams;</font>',
+        '&clubs;');
+    $values = array('A',2,3,4,5,6,7,8,9,10,'J','Q','K');
     echo '<table border="1" width="100%">';
     foreach ($players as $player){
+        sort($player);
         echo '<tr>';
         foreach ($player as $card){
             echo '<td>';
-            echo $card;
+            $pattern = (int)($card / 13);
+            $value = $card % 13;
+            echo "{$patterns[$pattern]}{$values[$value]}";
             echo '</td>';
         }
         echo '</tr>';
