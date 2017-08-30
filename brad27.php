6abdaa75b125a0e456c720b33c731fb93517fd8c
Brad <brad@brad.tw>
2017-08-30 16:34:26 +0800

    v26

---
+++ brad27.php
@@ -0,0 +1,35 @@
+<?php
+    date_default_timezone_set("Asia/Taipei");
+    $dir = @opendir(".") or exit('Server Busy');
+
+    while ($cont = readdir($dir)){
+
+        echo $cont . " : ";
+        if (is_dir("./{$cont}")){
+            echo "[DIR] : ";
+        }else if (is_file("./{$cont}")){
+            echo "[FILE] : ";
+        }
+
+        echo date('Y-m-d H:i:s' ,filemtime("./{$cont}"));
+
+        echo '<br>';
+
+    }
+
+    echo '<hr>';
+
+    if (@copy('./dir1/file1', './dir3/file2')){
+        echo 'Copy OK';
+    }else{
+        echo 'Copy Fail';
+    }
+
+
+
+
+
+
+
+
+    
