3d40c10f7c20347928faeb5cdf1313a0f78b19b6
Brad <brad@brad.tw>
2017-09-04 16:33:39 +0800

    v47

--- brad62.php
+++ brad62.php
@@ -1,2 +1,7 @@
 <?php
-echo rand(1,49);
+    $mysqli = new mysqli('localhost','root','root', 'iii');
+    $account = $_GET['account'];
+    $sql = "SELECT account FROM member WHERE account='{$account}'";
+    $result = $mysqli->query($sql);
+    echo $result->num_rows;
+
